8161bce4e68b27f88410d1280c67bf3fe5727429
sıla taş <silatass@gmail.com>
2021-02-27 23:54:39 +0300

    6 şubat

--- DataAccess/Abstract/IOrderDal.cs
+++ DataAccess/Abstract/IOrderDal.cs
@@ -6,8 +6,8 @@ using Entities.Concrete;
 
 namespace DataAccess.Abstract
 {
-	public interface ICategoryDal : IEntityRepository<Category>
+	public interface IOrderDal : IEntityRepository<Order>
 	{
-		
+
 	}
 }
